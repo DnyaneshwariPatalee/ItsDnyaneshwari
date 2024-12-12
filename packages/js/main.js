b23a695be845879e367fd31f337f260e7f593de2
koparkar555@gmail.com <dnyanupatale40@gmail.com>
2024-12-12 21:59:31 +0530

    Second commit

--- packages/js/main.js
+++ packages/js/main.js
@@ -116,6 +116,11 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
   grabCursor: true,
   spaceBetween: 48,
 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
